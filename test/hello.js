7a29381a4b8a8351deada7c45b0736396f2e2e59
JanNitschke <j.nitschke@videoboost.de>
2019-10-20 01:00:53 +0200

    removed test error, added acorde dep

--- test/hello.js
+++ test/hello.js
@@ -1,7 +1,7 @@
 
 exports.handle = async(msg, sendMessage) => {
     msg.res.body = "Hello world! req:"+ JSON.stringify(msg) ;
-    msg.res.header["test"] = "ja!";
+    msg.res.headers["test"] = "ja!";
     msg.res.status = 200;
 
     return msg;
